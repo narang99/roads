f85ec9f5ab62b5e2c171b13ab410a67a154bf73c
narang99 <hariom2711@gmail.com>
2026-01-31 11:29:03 +0530

    return stats in padded resize

--- mtrain/src/mtrain/smallnet/tfms.py
+++ mtrain/src/mtrain/smallnet/tfms.py
@@ -1,5 +1,5 @@
 # converting a cropped image from TACO to similar to road side images
-# this is the main module for make synthetic data that is 
+# this is the main module for make synthetic data that is
 # similar to actual real roadside street view images captured from cameras
 import cv2
 import functools
@@ -10,6 +10,11 @@ def PaddedResize(target_size):
 
 
 def resize_and_pad(img, target_size, pad_value=0):
+    padded, _ = resize_and_pad_raw(img, target_size, pad_value=0)
+    return padded
+
+
+def resize_and_pad_raw(img, target_size, pad_value=0):
     """
     Resize image so that the larger dimension becomes `target_size`,
     then pad the other dimension to get
@@ -30,25 +35,30 @@ def resize_and_pad(img, target_size, pad_value=0):
     new_w = int(round(w * scale))
     new_h = int(round(h * scale))
 
-    resized = cv2.resize(
-        img, (new_w, new_h), interpolation=cv2.INTER_AREA
-    )
+    resized = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
 
     # Compute padding
     pad_x = target_size - new_w
     pad_y = target_size - new_h
 
-    pad_left   = pad_x // 2
-    pad_right  = pad_x - pad_left
-    pad_top    = pad_y // 2
+    pad_left = pad_x // 2
+    pad_right = pad_x - pad_left
+    pad_top = pad_y // 2
     pad_bottom = pad_y - pad_top
 
     padded = cv2.copyMakeBorder(
         resized,
-        pad_top, pad_bottom,
-        pad_left, pad_right,
+        pad_top,
+        pad_bottom,
+        pad_left,
+        pad_right,
         borderType=cv2.BORDER_CONSTANT,
-        value=pad_value
+        value=pad_value,
     )
 
-    return padded
+    return padded, {
+        "left": pad_left,
+        "right": pad_right,
+        "top": pad_top,
+        "bottom": pad_bottom,
+    }
