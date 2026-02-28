861572fc43f245feb2063b79f8e1ae14cb506631
narang99 <hariom2711@gmail.com>
2026-02-28 11:50:17 +0530

    rm tqdm in predict

--- mtrain/src/mtrain/smallnet/unet/predict.py
+++ mtrain/src/mtrain/smallnet/unet/predict.py
@@ -66,15 +66,11 @@ def _do_batch(batch, learner):
 def predict_unet_only_mask(img_arr, sz, learner, bs):
     arr_and_coord = split_image_into_tiles(img_arr, sz)
     mask_and_coord = list(chain.from_iterable((
-        _do_batch(batch, learner) for batch in tqdm(batched(arr_and_coord, bs))
+        _do_batch(batch, learner) for batch in batched(arr_and_coord, bs)
     )))
-    # mask_and_coord = [
-    #     (learner.predict(arr)[0].numpy(), (y, x))
-    #     for (arr, (y, x)) in tqdm(arr_and_coord)
-    # ]
     H, W = img_arr.shape[:2]
     res = np.zeros((H, W), dtype=np.bool)
-    for mask, (y, x) in tqdm(mask_and_coord):
+    for mask, (y, x) in mask_and_coord:
         ny, nx = min(y + sz, H), min(x + sz, W)
         res[y:ny, x:nx] = mask[: ny - y, : nx - x].astype(bool)
 
